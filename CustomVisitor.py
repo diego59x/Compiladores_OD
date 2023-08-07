ad436d32981b36cb70c6bb53f0e865304ba6ce27
oscarsaravia <oscarsaraviard0013@gmail.com>
2023-08-07 15:46:39 -0600

    Avances

--- CustomVisitor.py
+++ CustomVisitor.py
@@ -4,5 +4,40 @@ from yapl.utils.Node import *
 class CustomVisitor(HelloVisitor):
     def __init__(self):
         self.errores = []
+    
     def visitInteger(self, ctx:HelloVisitor.IntegerContext):
         return IntNode(ctx.INT_VAR().getText())
+    
+    def visitBoolean(self, ctx:HelloVisitor.BooleanContext):
+        return BooleanNode(ctx.BOOLEAN_VAR().getText())
+    
+    def visitSum(self, ctx:HelloVisitor.SumContext):
+        return
+    
+    def visitMinus(self, ctx:HelloVisitor.MinusContext):
+        return
+    
+    def visitMultiply(self, ctx:HelloVisitor.MultiplyContext):
+        return
+    
+    def visitDivide(self, ctx:HelloVisitor.DivideContext):
+        return
+    
+    def visitGreaterThan(self, ctx:HelloVisitor.GreaterThanContext):
+        return
+    
+    def visitGreaterThanOrEqual(self, ctx:HelloVisitor.GreaterThanOrEqualContext):
+        return
+    
+    def visitLessThan(self, ctx:HelloVisitor.LessThanContext):
+        return
+    
+    def visitLessThanOrEqual(self, ctx:HelloVisitor.LessThanOrEqualContext):
+        return
+    
+    def visitEqual(self, ctx:HelloVisitor.EqualContext):
+        return
+    
+    def visitNot(self, ctx:HelloVisitor.NotContext):
+        return
+    
--- yapl/utils/Node.py
+++ yapl/utils/Node.py
@@ -1,8 +1,44 @@
 class Node():
     def __init__(self, token):
         self.token = token
+        self.type = "none"
+    def __str__(self):
+        return self.token
 
 class IntNode(Node):
     def __init__(self, token):
         self.token = token
-        self.type = "Integer"
+        self.type = "integer"
+
+class BooleanNode(Node):
+    def __init__(self, token):
+        self.token = token
+        self.type = "boolean"
+
+class SumNode(Node):
+    def __init__(self, left, right):
+        self.left = left
+        self.right = right
+        self.type = "sum"
+
+class IdNode(Node):
+    def __init__(self, token):
+        self.token = token
+        self.type = "id"
+
+class ErrorNode(Node):
+    def __init__(self, token):
+        self.token = token
+        self.error_list = []
+
+class ArithmeticNode(Node):
+    def __init__(self, left, right):
+        self.left = left
+        self.right = right
+        self.type = "arithmetic"
+
+class LogicalNode(Node):
+    def __init__(self, left, right):
+        self.left = left
+        self.right = right
+        self.type = "logical"
